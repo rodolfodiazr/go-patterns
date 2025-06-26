b9d9cd46a22c45b3d6f0326b237fe4d6f024c05e
Rodolfo Díaz <rodolfo.diaz.rovira@gmail.com>
2025-06-26 09:51:19 -0500

    Add a unit test for Strategy Pattern.

---
+++ behavioral/strategy/strategy_test.go
@@ -0,0 +1,33 @@
+package strategy
+
+import (
+	"testing"
+)
+
+type MockPayment struct {
+	called bool
+	amount float64
+}
+
+func (m *MockPayment) Pay(amount float64) {
+	m.amount = amount
+	m.called = true
+}
+
+func Test_PaymentMethod_Checkout(t *testing.T) {
+	m := &MockPayment{}
+	cart := &ShoppingCart{
+		Amount:  50.00,
+		Payment: m,
+	}
+
+	cart.Checkout()
+
+	if !m.called {
+		t.Errorf("Expected Pay Method to be called")
+	}
+
+	if m.amount != 50.00 {
+		t.Errorf("Expected amount = 50.00, got %v", m.amount)
+	}
+}
