4cf6e8c4c1f2ddc2bc2b348c9852816b9be62595
Rodolfo Díaz <rodolfo.diaz.rovira@gmail.com>
2025-07-08 09:59:15 -0500

    Update a unit test for Command Pattern.

--- behavioral/command/command_test.go
+++ behavioral/command/command_test.go
@@ -18,7 +18,16 @@ func TestScheduler_Run(t *testing.T) {
 	cmd2 := &MockCommand{}
 	scheduler.Add(cmd1)
 	scheduler.Add(cmd2)
+
+	if len(scheduler.queue) != 2 {
+		t.Errorf("Expected queue of 2 commands, got %d", len(scheduler.queue))
+	}
+
 	scheduler.Run()
+
+	if len(scheduler.queue) != 0 {
+		t.Errorf("Expected queue to be empty after run, got %d", len(scheduler.queue))
+	}
 
 	if !cmd1.Executed {
 		t.Error("Expected cmd1 to be executed")
